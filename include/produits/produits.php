662dbe1c4231286c08fd6041d26b13f9d3c722e8
champix56 <desorbaix@free.fr>
2022-10-18 15:18:39 +0200

    decoupage de l'app pour SPA

---
+++ include/produits/produits.php
@@ -0,0 +1,23 @@
+<h2>Liste des produits</h2>
+<hr/>
+<table class="produit-liste">
+    <thead>
+        <tr>
+            <th>image</th>
+            <th>nom</th>
+            <th>prix</th>
+            <th>action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="produit-liste-image"><img src="" alt=""></td>
+            <td class="produit-liste-nom">nom produit</td>
+            <td class="produit-liste-prix">24.50€</td>
+            <td>
+                <button type="button" class="btn btn-warning">add</button><br/>
+                <button type="button" class="btn btn-primary">voir</button>
+            </td>
+        </tr>
+    </tbody>
+</table>
