756a80a703f65058e597c92d51634fad3067b82d
champix56 <desorbaix@free.fr>
2022-10-18 15:52:20 +0200

    parcours de tableaux avec for

--- include/produits/produits.php
+++ include/produits/produits.php
@@ -1,5 +1,21 @@
 <h2>Liste des produits</h2>
 <hr/>
+<?php
+    //creation de l'array vide
+    $produits=[];
+
+    //creation d'un produit
+    $produit=['id'=>0,'nom'=>'vélo','prix'=>2000,'image'=>'https://lapierre-shopware.accell.cloud/thumbnail/64/bb/9f/1648474576/E-Sensium%202.2%20MY21%20Web%20-%20View%20PNG_800x800.png'];
+    
+    //affichage du contenu d'un array 
+    print_r($produit);
+    
+    //ajout dans l'array d'un produit plusieurs fois
+    array_push($produits,$produit,$produit);
+    
+    //affichage d'une instance simple ou complexe
+    var_dump($produits);
+?>
 <table class="produit-liste">
     <thead>
         <tr>
@@ -10,14 +26,20 @@
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td class="produit-liste-image"><img src="" alt=""></td>
-            <td class="produit-liste-nom">nom produit</td>
-            <td class="produit-liste-prix">24.50€</td>
-            <td>
-                <button type="button" class="btn btn-warning">add</button><br/>
-                <button type="button" class="btn btn-primary">voir</button>
-            </td>
-        </tr>
+       <?php
+       for ($i=0; $i <count($produits) ; $i++) { 
+            $pr=$produits[$i];
+            ?><tr>
+                <td class="produit-liste-image"><img src="<?= $pr['image'] ?>" alt=""></td>
+                <td class="produit-liste-nom"><?= $pr['nom']?></td>
+                <td class="produit-liste-prix"><?php echo $pr['prix'];?>€</td>
+                <td>
+                    <button type="button" class="btn btn-warning">add</button><br/>
+                    <button type="button" class="btn btn-primary">voir</button>
+                </td>
+            </tr>
+        <?php 
+         }
+        ?>
     </tbody>
 </table>
