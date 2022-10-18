067f98c0e78a125bde930944ab6f020d97be8233
champix56 <desorbaix@free.fr>
2022-10-18 20:02:36 +0200

    ecriture du SELECT produit

--- pages/produit/produit.functions.php
+++ pages/produit/produit.functions.php
@@ -1,6 +1,13 @@
 <?php
-    function getProduit($id){
-        
-    }
-
-?>
+include_once 'functions/sql/sql.php';
+/**
+ * get product all values
+ */
+function getProduit($id)
+{
+    global $mysqli;
+    sql_connect();
+    echo $req = "SELECT * FROM `produits` WHERE id=" . $id;
+    $returned = mysqli_query($mysqli, $req);
+    return mysqli_fetch_assoc($returned);
+}
