4b66f8ed63fad7b125a142c14f71d7fc840795b4
champix56 <desorbaix@free.fr>
2022-10-18 16:50:32 +0200

    base statique pour un produit unique

---
+++ include/produit/produit.php
@@ -0,0 +1,17 @@
+<div id="produit-unique">
+    <h2>TITRE DU PRODUIT</h2>
+    <hr/>
+    <div class="horizontal-layout">
+        <div class="produit-unique-image">
+            <img src="#" class="img-responsive" alt="Image">
+        </div>
+        <div class="produit-unique-center">
+            <h3>Description :</h3>
+            Excepteur laboris exercitation reprehenderit sunt amet elit quis voluptate. Nostrud Lorem minim sit consectetur laboris proident excepteur tempor. Officia veniam dolore sunt incididunt ut consequat quis nisi cillum eiusmod minim anim. Voluptate duis exercitation consequat et quis nisi laborum velit esse aliqua. Nisi irure occaecat ad exercitation cupidatat ex elit qui in ad culpa non dolor.
+            Cupidatat est occaecat aliquip ex fugiat excepteur. Est proident irure culpa culpa cupidatat. Lorem pariatur magna dolore excepteur. Duis excepteur labore elit officia elit.
+        </div>
+        <div class="produit-unique-buttons">
+            <button type="button" class="btn btn-info">Ajouter</button>
+        </div>
+    </div>
+</div>
