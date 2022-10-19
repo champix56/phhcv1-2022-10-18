386865e01a351ac45a8bf53474a9548eb00a4974
champix56 <desorbaix@free.fr>
2022-10-19 10:02:58 +0200

    selection mysqli plusieurs produits

--- include/pages/produits/produits.functions.php
+++ include/pages/produits/produits.functions.php
@@ -3,6 +3,7 @@
 $mysqli = mysqli_connect("localhost:3307", "root", "", "phh-22-10-18");
 $req="SELECT P.`id` AS `pid`, `id_categories`, P.`nom` AS `pnom`, `EAN`, `prix`, `description`, `image`, C.`nom` AS `cnom` FROM `produits` P, `categories` C WHERE P.`id_categories` = C.`id`";// AND C.`id`=1;";
 $result = mysqli_query($mysqli, $req);
-$assoc=mysqli_fetch_assoc($result);
-var_dump($assoc);
+while($assoc=mysqli_fetch_assoc($result)){
+    var_dump($assoc);
+}
 ?>
