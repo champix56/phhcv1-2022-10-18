73d241a23fb526e0b940f745b86199c3fb96f18c
champix56 <desorbaix@free.fr>
2022-10-19 15:26:12 +0200

    functions pour acces aux categories

---
+++ include/pages/categories/categories.functions.php
@@ -0,0 +1,22 @@
+<?php
+include_once 'include/functions/sql.php'
+/**
+ * fonction pour recuperer un tableau de categories
+ */
+function getCategories(){
+    //recup descripteur $mysqli
+    global $mysqli;
+    //requete
+    $req="SELECT `id`,`nom` FROM `categories`";
+    //query
+    $result = mysql_query($mysql,$req);
+    //creer tableau
+    $return_tab=[];
+    //pour chaque  fetch_assoc pousser la ligne dans le tableau
+    while ($categorie = mysqli_fetch_assoc($result)) {
+        array_push($return_tab,$categorie);
+    }
+    //retourner le tableau rempli
+    return $return_tab;
+}
+?>
