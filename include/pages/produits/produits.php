6d332e55462b9ffa031a512bc2e35def84ba8844
champix56 <desorbaix@free.fr>
2022-10-19 10:24:39 +0200

    function avec retour

--- include/pages/produits/produits.php
+++ include/pages/produits/produits.php
@@ -3,21 +3,10 @@
 <?php
 
 include_once 'produits.functions.php ';
-var_dump($req);
-//creation de l'array vide
-$produits = [];
 
-//creation d'un produit
-$produit = ['id' => 0, 'nom' => 'vélo', 'prix' => 2000, 'image' => 'https://lapierre-shopware.accell.cloud/thumbnail/64/bb/9f/1648474576/E-Sensium%202.2%20MY21%20Web%20-%20View%20PNG_800x800.png'];
-
-//affichage du contenu d'un array
-print_r($produit);
-
-//ajout dans l'array d'un produit plusieurs fois
-array_push($produits, $produit, $produit);
-
-//affichage d'une instance simple ou complexe
+$produits = getProduits();
 var_dump($produits);
+
 ?>
 <table class="produit-liste">
     <thead>
