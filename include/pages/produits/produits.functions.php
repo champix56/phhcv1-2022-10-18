c3df0984f7157d8a7fe18d43193668873ab7ba79
champix56 <desorbaix@free.fr>
2022-10-19 10:00:56 +0200

    selection mysqli d'un produit

---
+++ include/pages/produits/produits.functions.php
@@ -0,0 +1,8 @@
+<?php
+
+$mysqli = mysqli_connect("localhost:3307", "root", "", "phh-22-10-18");
+$req="SELECT P.`id` AS `pid`, `id_categories`, P.`nom` AS `pnom`, `EAN`, `prix`, `description`, `image`, C.`nom` AS `cnom` FROM `produits` P, `categories` C WHERE P.`id_categories` = C.`id`";// AND C.`id`=1;";
+$result = mysqli_query($mysqli, $req);
+$assoc=mysqli_fetch_assoc($result);
+var_dump($assoc);
+?>
