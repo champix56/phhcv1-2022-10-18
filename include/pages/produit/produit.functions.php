a006a1c06721e60276c0ca38f031605d04488b20
champix56 <desorbaix@free.fr>
2022-10-20 11:17:44 +0200

    execute_query dispo que des php>=8.2

--- include/pages/produit/produit.functions.php
+++ include/pages/produit/produit.functions.php
@@ -17,8 +17,18 @@ function postProduit($idcat,$nom,$ean,$prix,$description,$img){
 }
 function putProduit($id,$idcat,$nom,$ean,$prix,$description,$img){
     global $mysqli;
-    $req="UPDATE `produits` SET `id_categories`=?,`nom`='?',`EAN`='?',`prix`='?',`description`='?',`image`='?' WHERE `id`=?";
-    $result=mysqli_execute_query($mysqli,$req,[$idcat,$nom,$ean,$prix,$description,$img,$id]);
+    $req="UPDATE `produits` SET `id_categories`=$idcat,`nom`='$nom',`EAN`='$ean',`prix`=$prix,`description`='$description',`image`='$img' WHERE `id`=$id";
+    var_dump($req);
+    mysqli_query($mysqli,$req);
+    /*
+    $stmt = mysqli_prepare($mysqli, "UPDATE `produits` SET `id_categories`=?,`nom`=?,`EAN`=?,`prix`=?,`description`=?,`image`=? WHERE `id`=?");
+    mysqli_stmt_bind_param($stmt, 'issfssi',$idcat,$nom,$ean,$prix,$description,$img,$id);
+    mysqli_stmt_execute($stmt);
+    */
+
+    
+    //$result=mysqli_execute_query($mysqli,$req,[$idcat,$nom,$ean,$prix,$description,$img,$id]);
+
     return  mysqli_affected_rows($mysqli)==1;
 }
 ?>
