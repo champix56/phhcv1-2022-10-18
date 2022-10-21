163835c687603a894103b3f5ed1a802db6f4857d
champix56 <desorbaix@free.fr>
2022-10-21 14:27:56 +0200

    passage Obj du form

--- include/pages/produit_proc/produit.form.php
+++ include/pages/produit_proc/produit.form.php
@@ -5,11 +5,11 @@ if(isset($_POST['idProduit'])){
     //soumission du formulaire
     if(is_numeric($_POST['idProduit']))
     {
-        putProduit($_POST['idProduit'],$_POST['categorieProduit'],$_POST['nomProduit'],$_POST['eanProduit'],$_POST['prixProduit'],$_POST['descriptionProduit'],$_POST['urlImageProduit']);
+        putProduit(new Produit($_POST['idProduit'],$_POST['nomProduit'],$_POST['descriptionProduit'],$_POST['prixProduit'],$_POST['eanProduit'],$_POST['urlImageProduit'],$_POST['categorieProduit']));
     }
     else{
-       $id=postProduit($_POST['categorieProduit'],$_POST['nomProduit'],$_POST['eanProduit'],$_POST['prixProduit'],$_POST['descriptionProduit'],$_POST['urlImageProduit']);
-       header('Location:?page=produit&action=edit&idp='.$id);
+       $pr=postProduit(new Produit($_POST['idProduit'],$_POST['nomProduit'],$_POST['descriptionProduit'],$_POST['prixProduit'],$_POST['eanProduit'],$_POST['urlImageProduit'],$_POST['categorieProduit']));
+       header('Location:?page=produit&action=edit&idp='.$pr->getId());
     }
 }
 
@@ -19,16 +19,16 @@ if (isset($_GET['idp'])) {
     $produit = getProduit($_GET['idp']);
 }
 ?>
-<div id="produit-form" style=";padding:5px 25px;">
+<div id="produit-form" style="padding:5px 25px;">
     <h2>Edition produit</h2>
     <form action="" method="POST">
         <?php
-if ($produit != null) {echo 'id:' . $produit['id'] . '<br/>';}
+if ($produit != null) {echo 'id:' . $produit->getId() . '<br/>';}
 ?>
-        <input type="hidden" name="idProduit" value="<?=($produit != null) ? $produit['id'] : ''?>">
+        <input type="hidden" name="idProduit" value="<?=($produit != null) ? $produit->getId() : ''?>">
         <label for="nomProduit">Nom produit :</label>
         <input type="text" name="nomProduit" id="i_nomProduit"
-            value="<?=($produit != null) ? $produit['nom'] : ''?>"><br />
+            value="<?=($produit != null) ? $produit->getNom() : ''?>"><br />
         <label for="categorieProduit">Categorie</label>
         <select name="categorieProduit" id="i_categorieProduit">
             <?php
@@ -38,7 +38,7 @@ foreach ($categories as $cat) {
     echo '<option value="'.
             $cat['id'].
             '" '.
-            (($produit != null && $produit['id_categories']==$cat['id']) ? 'selected' : '').
+            (($produit != null && $produit->getIdcat()==$cat['id']) ? 'selected' : '').
             '>' .
             $cat['nom'] .
             '</option>';
@@ -50,20 +50,20 @@ foreach ($categories as $cat) {
             <div style="width:60%">
                 <label for="descriptionProduit">Description</label><br />
                 <textarea name="descriptionProduit" id="i_descriptionProduit" cols="30" rows="10"
-                    style="margin-left:15%;width:70%;resize : none;"><?=($produit != null) ? $produit['description'] : ''?></textarea><br />
+                    style="margin-left:15%;width:70%;resize : none;"><?=($produit != null) ? $produit->description : ''?></textarea><br />
                 <label for="eanProduit">barcode</label><br />
                 <input type="text" name="eanProduit" id="i_eanProduit"
-                    value="<?=($produit != null) ? $produit['EAN'] : ''?>"><br />
+                    value="<?=($produit != null) ? $produit->getEAN() : ''?>"><br />
                 <label for="prixProduit">Prix</label><br />
                 <input type="number" name="prixProduit" id="i_prixProduit" min="0.01" step="0.01"
-                    value="<?=($produit != null) ? $produit['prix'] : ''?>">
+                    value="<?=($produit != null) ? $produit->getPrix() : ''?>">
             </div>
             <div style="flex-grow:1;padding:5px 15px;text-align:center">
                 <label for="urlImageProduit">url image</label><br />
                 <input type="text" name="urlImageProduit" id="i_urlImageProduit" style="max-width:45vw;max-height:45vh;"
-                    value="<?=($produit != null) ? $produit['image'] : ''?>">
+                    value="<?=($produit != null) ? $produit->getImage() : ''?>">
                 <hr />
-                <img src="<?=($produit != null) ? $produit['image'] : ''?>" alt="" id="imageProduit">
+                <img src="<?=($produit != null) ? $produit->getImage() : ''?>" alt="" id="imageProduit">
             </div>
         </div>
         <hr />
